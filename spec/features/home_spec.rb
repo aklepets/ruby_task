211bb49fb3ecd2cd3bbb761747d25fe0429c8438
Andrei Klepets <a.klepets@unlitsolutions.com>
2017-03-17 19:01:35 +0300

    add webkit to Gemfile, edit home_spec, edit .rspec

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -4,7 +4,8 @@ require 'rails_helper'
 
 	feature "Loging in to website" do
 		scenario "Input login, pass and click submit" do
-			visit root_path
+			visit 'http://dev.by'
+      binding.pry
 			fill_in('username', :with => 'testdvb')
 			fill_in('password', :with => 'testdvb1234')
 			click_button('Log in')
